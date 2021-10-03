9b3ba70832fd08cc4c24ced1779b7cd8f63c694a
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2021-10-03 20:37:02 +0200

    Done Launch

---
+++ Space Quest 2021/Assets/buds.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buds : MonoBehaviour
+{
+    [SerializeField]
+    bool showObjective = false;
+    [SerializeField]
+    Texture objective;
+    [SerializeField]
+    private int collision;
+    public int scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        showObjective = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && showObjective == false && collision == 0)
+            showObjective = true;
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            FadeManager.instance.FadeOutToScene(scene);
+        }
+    }
+    void OnGUI()
+    {
+        if (showObjective == true)
+            GUI.DrawTexture(new Rect(Screen.width / 1.5f, Screen.height / 1.4f, 300, 300), objective);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("ShowObj") && collision == 1 )
+        {
+            showObjective = true;
+        }
+        if (Input.GetButtonUp("ShowObj") && collision == 1)
+        {
+            showObjective = false;
+        }
+    }
+}
