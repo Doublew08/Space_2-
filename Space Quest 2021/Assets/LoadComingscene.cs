263ef3df5f43512943306cc92de7be4fdcb705ba
Doublew08 <86314168+Doublew08@users.noreply.github.com>
2021-10-03 21:38:38 +0200

    done scene load

---
+++ Space Quest 2021/Assets/LoadComingscene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadComingscene : MonoBehaviour
+{
+    public int sceneload;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Enumerator()
+    {
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene(sceneload);
+    }
+}
