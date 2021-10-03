cf16e8a801ac23b70d138facd8032fbf6c1d3c47
CR7H1000 <35869363+CR7H1000@users.noreply.github.com>
2021-10-03 21:16:15 +0200

    final

---
+++ Space Quest 2021/Assets/loadnextscene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class loadnextscene : MonoBehaviour
+{
+    private int loadnextscene21;
+    void Start()
+    {
+        loadnextscene21 = SceneManager.GetActiveScene().buildIndex +1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
